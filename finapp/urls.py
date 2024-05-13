2a973396857b26fc5fe3dbe1a679bd6f04889b7c
melatTassefa <99674626+melatTassefa@users.noreply.github.com>
2024-05-13 13:35:21 +0000

    search functionality added

--- finapp/urls.py
+++ finapp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
-from .views import contact
+from .views import contact,aboutus,login,registration, college_search
 urlpatterns = [
     path('contact/', contact, name="contact_url"),
+    path('aboutus/', aboutus, name="aboutus_url"),
+    path('login/', login, name="login_url"),
+    path('registration/', registration, name="registration_url"),
+    path('search/', college_search, name="college_search_url")
 ]
--- finapp/views.py
+++ finapp/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def aboutus(request):
+    return render(request, 'aboutus.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def registration(request):
+    return render(request, 'registration.html')
+
+def college_search(request):
+    return render(request, 'college_search.html')
